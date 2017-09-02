300ca7f3e0ea497fc07296162c8a7477fe605823
William Emerison Six <billsix@gmail.com>
2017-09-02 15:32:46 -0500

    use opengl 1.4 for legacy

--- src/main.book.cpp
+++ src/main.book.cpp
@@ -211,8 +211,8 @@ int main(int argc, char *argv[])
   /*                     SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); */
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                       SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
 
   SDL_DisplayMode current;
   SDL_GetCurrentDisplayMode(0, &current);
