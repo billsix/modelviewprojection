1b21ae72d71f0ff39a7817852f525e136cac86cd
William Emerison Six <billsix@gmail.com>
2019-10-20 17:14:21 -0500

    start inlining the book into the code

--- modelviewprojection1.py
+++ modelviewprojection1.py
@@ -1,3 +1,55 @@
+# "I had no idea how much math was involved in computer graphics."
+
+# Unfortunately many students of computer graphics have the impression
+# that the writing of computer graphics programs requires knowledge of
+# advanced math; which is patently untrue.
+# Only the understanding of high-school level geometry is required.
+# Using math you already know, this book builds both 2D and 3D
+# applications from the ground up using OpenGL, a standard for graphics
+# programming.
+
+# Thoughout the book, I show how to place objects in space,
+# how to draw objects relative to other objects, how to add a
+# camera which moves over time based on user input, and how to transform all
+# the objects into the 2D pixel coordinates of the computer screen.
+# By the end of this book, you will understand the basics of
+# how to create first-person and third-person applications/games.
+# I made this book to show programmers how to make the kind
+# of graphics programs which they want to make, using
+# math they aleady know.
+
+# This book is purposely limited in scope, and
+# the applications produced are not particurly pretty nor realistic-looking.
+# For advanced graphics topics, you'll need to consult other references,
+# such as the OpenGL "red book" and "blue book".
+# Although this book fills a huge gap that other books do not address,
+# those other books are excellent reference books for advanced topics.
+
+###### Basics
+#
+# The device attached to a computer which displays information to the user is called a *monitor*.
+# The monitor is composed of a two-dimensional array of light-emitting elements, each called a *pixel*.
+# At a given time, each individual pixel is instructed to display
+# one specific color, represented within the computer as a number.
+# The aggregate of the colors at each pixel at one moment in time, called a *frame*,
+# provides a picture that has some meaning to the human user.
+#
+# In OpenGL, the bottom left pixel of a window is coordinate (0,0).  The top right is (window_width,window_height)
+#
+# 1024x768 monitor
+# eog monitor.png
+#
+# 1920x1200 monitor
+# eog monitor2.png
+#
+#
+# Frames are created within the computer and sent to the monitor
+# at a rate over time, called the *framerate*,
+# measured in *Hertz*.  By updating frames quickly and at a constant rate, the computer
+# provides the end-user with the illusion of motion.
+#
+# TODO - insert 3 20x20 frames which show motion
+
 import sys
 import os
 import numpy as np
@@ -5,12 +57,45 @@ import math
 from OpenGL.GL import *
 import glfw
 
+##### Opening a Window
+#
+# Desktop operating systems allow the user to run more than one
+# program at a time, where each program draws into a subsection of
+# the monitor called a window.
+#
+#
+# To create and to open a window in a cross-platform manner, this
+# book will call procedures provided by the widely-ported GLFW library (supporting Windows, macOS, Linux).
+# GLFW also provides procedures for receiving
+# keyboard input and for controller.
+#
+
+
+###### GLFW/OpenGL Initialization
+#
+# -Initialize GLFW.
+#
 if not glfw.init():
     sys.exit()
-
+# One frame is created incrementally over time on the CPU, but the frame
+# is sent to the monitor
+# only when frame is completely drawn, and each pixel has a color.
+# The act of sending the frame to the monitor is called *flushing*
+# the frame.
+# Flushing takes time,
+# and if the call to flush were to blockfootnote:[meaning it would not return control back to the call-ing procedure until the flush is complete], we would
+# have wasted CPU time.  To avoid this,
+# OpenGL has two *framebuffers*footnote:[regions of memory which will eventually contain the full data for a frame],
+# only one of which is "active", or writable, at a given time.
+# "glfwSwapBuffers" is a non-blocking call which initiates the flushing
+# the current buffer, and which switches the current writable framebuffer to the
+# other one, thus allowing the CPU to resume.
+#
+# -Set the version of OpenGL
+# OpenGL has been around a long time, and has multiple, possibly incompatible versions.
 glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR,1)
 glfw.window_hint(glfw.CONTEXT_VERSION_MINOR,4)
-
+# Create a 500 pixel by 500 pixel window, which the user can resize.
 window = glfw.create_window(500,
                             500,
                             "ModelViewProjection Demo 1",
@@ -29,13 +114,19 @@ def on_key(window, key, scancode, action, mods):
         glfw.set_window_should_close(window,1)
 glfw.set_key_callback(window, on_key)
 
+# For every frame drawn, each pixel has a default color, set by
+# calling "glClearColor". "0,0,0,1", means black "0,0,0", without
+# transparency (the "1").
 glClearColor(0.0,
              0.0,
              0.0,
              1.0)
 
+#Enable blending of new values in a fragment with the old value.
 glEnable(GL_BLEND)
 
+# Specify how a given fragment's color value within the framebuffer combines with a second color.  This new
+#blended value is then set for the fragment.
 glBlendFunc(GL_SRC_ALPHA,
             GL_ONE_MINUS_SRC_ALPHA)
 
@@ -43,6 +134,33 @@ glMatrixMode(GL_PROJECTION);
 glLoadIdentity();
 glMatrixMode(GL_MODELVIEW);
 glLoadIdentity();
+
+
+
+###### The Event Loop
+#
+# When you pause a movie, motion stops and you see one picture.
+# Movies are composed of sequence of pictures, when
+# rendered in quick succession, provide the illusion of motion.
+#
+# Interactive computer graphics are rendered the same way,
+# one "frame" at a time.
+#
+# Render a frame, flush the complete frame to the monitor.
+# Unless the user closed the window, repeat indefinitely.
+#
+# The color of each pixel withith
+# the current framebuffer
+# is reset to a default color.
+#
+# When a graphics application is executing, it is creating new
+# frames (pictures) at some rate (e.g. 60 frames per second).  At any given
+# frame, the user of the application might do something, (e.g.
+# move the mouse, click, type on the keyboard, close the application).
+#
+# At the beginning of every frame, ask GLFW if it received one
+# of these events since we last asked (i.e., the previous frame).
+
 
 # Loop until the user closes the window
 while not glfw.window_should_close(window):
@@ -61,3 +179,47 @@ while not glfw.window_should_close(window):
     glfw.swap_buffers(window)
 
 glfw.terminate()
+
+
+###### Black Screen
+#
+# Add the "mvp" direction to your PYTHONPATH, and type
+# "python modelviewprojection.py". When prompted, type "1" and then press the "Enter" key.
+#
+# The first demo is the least interesting graphical program possible.
+#
+# -Sets the color at every pixel black.  (A constant color is better than whatever
+# color happened to be the previous time it was drawn.)
+#
+# -If the user resized the window, reset OpenGL's mappings from *normalized-device-coordinates*
+# to *screen-coordinates*.
+#
+# -Cleared the depth buffer (don't worry about this for now).
+#
+# When this code returns, the event loop flushes (i.e) sends the frame to the monitor.  Since
+# no geometry was drawn, the color value for each pixel is still black.
+
+# Each color is represented by a number, so the frame is something like this:
+
+
+# ....
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# ....
+
+
+
+
+# The event loop then calls this code over and over again, and since we retain no state and
+# we draw nothing, a black screen will be displayed every frame until the user
+# closes the window, and says to himself, "why did I buy Doom 3"?
--- modelviewprojection2.py
+++ modelviewprojection2.py
@@ -1,3 +1,24 @@
+# == Draw Paddles
+
+
+# A black screen is not particularly interesting, so
+# let's draw something, say, two rectangles.
+# Where should they be, and what color should they be?
+
+# "glColor3f" sets a global variable, which makes it the color to be used
+# for the subsequently-drawn graphical shape.  The background will be black,
+# so lets make the first paddle purple, and a second paddle red.
+
+# "glBegin(GL_QUADS)" tells OpenGL that we will soon specify 4 *vertices*,
+# (i.e. points) which define the
+# quadrilateral.  The vertices will be specified by calling "glVertex2f" 4 times.
+
+# "glEnd()" tells OpenGL that we have finished providing vertices for
+# the begun quadrilateral.
+
+
+
+
 import sys
 import os
 import numpy as np
@@ -68,6 +89,11 @@ while not glfw.window_should_close(window):
     glVertex2f(-1.0, #x
                0.3)  #y
     glEnd()
+
+    # The framebuffer, which has not yet been flushed to the monitor, has geometry which looks like this:
+    # eog plot1.png
+
+
     # draw paddle 2
     glColor3f(1.0,
               0.0,
@@ -84,9 +110,97 @@ while not glfw.window_should_close(window):
                0.3)
     glEnd()
 
+    # The framebuffer, which has not yet been flushed to the monitor, has geometry which looks like this:
+    # eog plot2.png
+
 
     # done with frame, flush and swap buffers
     # Swap front and back buffers
     glfw.swap_buffers(window)
 
 glfw.terminate()
+
+
+
+# The frame sent to the monitor is a set of values like this:
+# ....
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# pppppbbbbbbbbbbbbbbbbbbbbbbbbbbbrrrrr
+# pppppbbbbbbbbbbbbbbbbbbbbbbbbbbbrrrrr
+# pppppbbbbbbbbbbbbbbbbbbbbbbbbbbbrrrrr
+# pppppbbbbbbbbbbbbbbbbbbbbbbbbbbbrrrrr
+# pppppbbbbbbbbbbbbbbbbbbbbbbbbbbbrrrrr
+# pppppbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+# ....
+
+# What do we have to do to convert from normalized-device-coordinates
+# into individual colors for each pixel?  Nothing, OpenGL does that for us; therefore
+# we never have to think in terms of pixels, only in terms of vertices of shapes,
+# specified by normalized-device-coordinates.
+
+# Why is that desirable?
+
+####### Normalized-Device-Coordinates
+
+# The author owns two monitors, one which has 1024x768 pixels, and one which has
+# 1920x1200 pixels.  When he purchases a game from Steam, he expects that his game
+# will run correctly on either monitor, in full-screen mode.  If a graphics programmer
+# had to explictly set each indiviual pixel's color, the the programmer would have to
+# program using "screen-space"footnote:[Any "space" means a system of numbers which you're using.
+# Screen-space means you're specifically using pixel coordinates, i.e, set pixel (5,10) to be red].
+
+# What looks alright is screen-space on a large monitor...
+# eog screenspace2.png
+
+# isn't even the same picture on a smaller monitor.
+# eog screenspace.png
+
+
+
+# Like any good program or library, OpenGL abstracts over screen-space, thus freeing the
+# programmer from caring about screen size.  If a programmer does not want to program
+# in discretefootnote:[discrete means integer values, not continuous] screen-space,
+# what type of numbers should he use?  Firstly, it should be a continuous space, meaning
+# that it should be in decimal numbers.  Because if a real-world object is 10.3 meters long, a programmer
+# should be able to enter "float foo = 10.3".  Secondly, it should be a fixed range vertically
+# and an fixed range horizontally.  OpenGL will have to convert points from some space to screen-space,
+# and since OpenGL does this in hardware (i.e. you can't programmatically change how the conversion
+# happens), it should be a fixed size.
+
+# OpenGL uses *normalized-device-coordinates* footnote:[normalized- meaning a distance value of
+# 1; device- the monitor; coordinates- the system of numbers (i.e. space) in which you are working],
+# which is a continous space from -1.0 to 1.0 horizontally,
+# and -1.0 to 1.0 vertically.
+
+
+# By specifying geometry using normalized-device-coordinates,
+# OpenGL will automatically convert from a continuous, -1.0 to 1.0 space,
+# to discrete pixel-space.
+
+# eog ndcSpace.png
+
+# Whether we own a small monitor
+
+# eog ndcSpace1.png
+
+# or a large monitor.
+
+# eog screenspace2.png
+
+
+# -Exercise 1.  The window is resizable by the user while it runs.
+# Do the paddles both  still appear in the window if you make it really thin?  What if
+# you make it very wide?
+
+
+# Answer - (Regardless of the window's width to height ratio, the pixel in the upper left of
+# the window still maps to normalized-device-coordinate (-1.0,-1.0), and the pixel
+# in the bottom right of the window still maps to (1.0,1.0).
+
+# -Exercise 2.  How would you convert from ndc-space to screen-space, given
+# a monitor width _w_ and height _h_?
