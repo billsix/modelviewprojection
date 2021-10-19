853ad564f07bbad1c027aa399b74dfc628472306
William Emerison Six <billsix@gmail.com>
2021-10-18 20:47:50 -0400

    added screenshots

--- src/demo16/demo.py
+++ src/demo16/demo.py
@@ -36,7 +36,7 @@ if not glfw.init():
 glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 1)
 glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 4)
 
-window = glfw.create_window(500, 500, "ModelViewProjection Demo 14", None, None)
+window = glfw.create_window(500, 500, "ModelViewProjection Demo 16", None, None)
 if not window:
     glfw.terminate()
     sys.exit()
