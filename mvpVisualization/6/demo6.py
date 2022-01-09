c37776445a9aa53a59c080c1056972756669fe14
William Emerison Six <billsix@gmail.com>
2022-01-09 15:16:46 -0500

    no need for file menu

--- mvpVisualization/6/demo6.py
+++ mvpVisualization/6/demo6.py
@@ -956,16 +956,6 @@ while not glfw.window_should_close(window):
 
     imgui.new_frame()
 
-    if imgui.begin_main_menu_bar():
-        if imgui.begin_menu("File", True):
-            clicked_quit, selected_quit = imgui.menu_item("Quit", "Cmd+Q", False, True)
-
-            if clicked_quit:
-                exit(0)
-
-            imgui.end_menu()
-        imgui.end_main_menu_bar()
-
     imgui.set_next_window_bg_alpha(0.05)
     imgui.begin("Time", True)
 
