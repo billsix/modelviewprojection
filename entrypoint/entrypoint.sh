3836f9c0c38315599a14846261d42e19e66b2efc
William Emerison Six <billsix@gmail.com>
2024-07-14 08:36:59 -0400

    not sure what is wrong

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -5,29 +5,3 @@ make html
 mkdir /output/modelviewprojection
 cp -r _build/html /output/modelviewprojection/
 
-cd /output/modelviewprojection/
-rm -rf static images sources
-mv _sources sources
-mv _images images
-mv _static static
-
-# For MacOS to not have sed complain
-export LC_CTYPE=C
-export LANG=C
-
-find . -iname '*.rst.txt' | xargs sed -i -e 's@_static/@static/@g'
-find . -iname '*.rst.txt' | xargs sed -i -e 's@_images/@images/@g'
-find . -iname '*.rst.txt' | xargs sed -i -e 's@_sources/@sources/@g'
-
-find . -iname '*.html' | xargs sed -i -e 's@_static/@static/@g'
-find . -iname '*.html' | xargs sed -i -e 's@_images/@images/@g'
-find . -iname '*.html' | xargs sed -i -e 's@_sources/@sources/@g'
-
-find . -iname '*.inv' | xargs sed -i -e 's@_static/@static/@g'
-find . -iname '*.inv' | xargs sed -i -e 's@_images/@images/@g'
-find . -iname '*.inv' | xargs sed -i -e 's@_sources/@sources/@g'
-
-find . -iname '*.js' | xargs sed -i -e 's@_static/@static/@g'
-find . -iname '*.js' | xargs sed -i -e 's@_images/@images/@g'
-find . -iname '*.js' | xargs sed -i -e 's@_sources/@sources/@g'
-
