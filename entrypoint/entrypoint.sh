e2bd594e1f4666f5e7c79f0550519b7259caf618
William Emerison Six <billsix@gmail.com>
2024-08-31 08:20:01 -0400

    put in the github fixes again

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -6,6 +6,26 @@ make html
 
 # fix issues that github has for displaying the pages for me
 cd _build/html/
+rm -rf static images sources
+mv _sources sources
+mv _images images
+mv _static static
+
+find . -iname '*.rst.txt' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.rst.txt' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.rst.txt' | xargs sed -i -e 's@_sources/@sources/@g'
+
+find . -iname '*.html' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.html' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.html' | xargs sed -i -e 's@_sources/@sources/@g'
+
+find . -iname '*.inv' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.inv' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.inv' | xargs sed -i -e 's@_sources/@sources/@g'
+
+find . -iname '*.js' | xargs sed -i -e 's@_static/@static/@g'
+find . -iname '*.js' | xargs sed -i -e 's@_images/@images/@g'
+find . -iname '*.js' | xargs sed -i -e 's@_sources/@sources/@g'
 
 
 # copy the files over
