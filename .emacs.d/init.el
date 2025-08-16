2fb488e343b7f6de548fb337ba07690d4b7e75e6
William Emerison Six <billsix@gmail.com>
2025-08-16 00:26:07 -0400

    lsp mode for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,3 +7,18 @@
 (load-theme 'modus-vivendi t)
 (global-auto-revert-mode)
 (setq auto-revert-avoid-polling t)
+
+;; Disable eglot hooks for python-mode
+(remove-hook 'python-mode-hook 'eglot-ensure)
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (expected by lsp-ui)
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((python-mode . (lambda ()
+                          (require 'lsp-mode)
+                          (lsp-deferred)))
+         (lsp-mode . yas-minor-mode))
+  :config
+  (setq lsp-pylsp-server-command "pylsp")
+  )
