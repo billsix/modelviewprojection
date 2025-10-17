04f43a19f14e090159b1f7e21ae8b5dec92d2205
William Emerison Six <billsix@gmail.com>
2025-10-16 21:46:47 -0400

    add intermediate plots in the notebook

--- assignments/demo02/plot2d.py
+++ assignments/demo02/plot2d.py
@@ -47,6 +47,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker
 import numpy as np
 
+import modelviewprojection.mathutils as mu
 import modelviewprojection.mathutils2d as mu2d
 
 extraLinesMultiplier = 3
@@ -88,9 +89,9 @@ def generategridlines(graphBounds, interval=1):
 
 
 @contextlib.contextmanager
-def create_graphs(title, filename, graph_bounds):
+def create_graphs(graph_bounds=(3, 3), title=None, filename=None):
     try:
-        fig, axes = plt.subplots(figsize=(6, 6))
+        fig, axes = plt.subplots(figsize=(3, 3))
         axes.set_xlim([-graph_bounds[0], graph_bounds[0]])
         axes.set_ylim([-graph_bounds[1], graph_bounds[1]])
 
@@ -107,9 +108,13 @@ def create_graphs(title, filename, graph_bounds):
 
 
 # %%
-
-
-def create_basis(graph_bounds, gridline_interval, fn=lambda x: x):
+def create_basis(
+    fn=lambda x: x,
+    graph_bounds=(10, 10),
+    gridline_interval=1,
+    xcolor=(0.0, 0.0, 1.0),
+    ycolor=(1.0, 0.0, 1.0),
+):
     # plot transformed basis
     for vecs, thickness in generategridlines(
         graph_bounds, interval=gridline_interval
@@ -130,7 +135,7 @@ def create_basis(graph_bounds, gridline_interval, fn=lambda x: x):
             [fn(vec).y for vec in x_axis],
             "-",
             lw=1.0,
-            color=(0.0, 0.0, 1.0),
+            color=xcolor,
         )
 
         # y axis
@@ -140,37 +145,61 @@ def create_basis(graph_bounds, gridline_interval, fn=lambda x: x):
             [fn(vec).y for vec in y_axis],
             "-",
             lw=1.0,
-            color=(1.0, 0.0, 1.0),
+            color=ycolor,
         )
 
 
-# %%
-
-with create_graphs("foo", "bar.txt", (10, 10)):
-    create_basis((3, 3), 1)
-
-# %%
-
-with create_graphs(
-    "foo",
-    "bar.txt",
-    (3, 3),
-):
-    create_basis((3, 3), 1, mu2d.rotate(math.radians(53.130102)))
+# %% [markdown]
+# Draw graph paper
+# ----------------
+#
+# Just draw graph paper, where one unit in the x direction is blue,
+# and one unit in the y direction is pink.  The graph paper corresponds
+# to the numbers on the left and on the bottom.
+#
+#
 
 # %%
-
-
-with create_graphs("foo", "bar.txt", (3, 3)):
-    create_basis((3, 3), 1, mu2d.rotate(0.0))
-    create_basis((3, 3), 1, mu2d.rotate(math.radians(53.130102)))
+with create_graphs():
+    create_basis()
 
 # %%
-with create_graphs("foo", "bar.txt", (3, 3)):
+
+with create_graphs():
+    create_basis(fn=mu2d.rotate(math.radians(53.130102)))
+
+# %% [markdown]
+# Draw relative graph paper
+# -------------------------
+#
+# Draw two relative number lines, making a relative graph paper,
+# but keep the original coordinate system on the left and bottom.
+# Any point in the plane can be described using two different
+# graph papers.
+#
+#
+
+# %%
+with create_graphs():
+    create_basis(fn=mu2d.rotate(0.0))
     create_basis(
-        (3, 3),
-        1,
-        mu2d.compose(
+        fn=mu2d.rotate(math.radians(53.130102)),
+        xcolor=(0, 1, 0),
+        ycolor=(0, 1, 1),
+    )
+
+# %% [markdown]
+# Draw relative graph paper, defined by composed functions
+# --------------------------------------------------------
+#
+# Draw a translated and rotated graph paper.
+# You can read the sequence of composed functions
+# in the order that they are applied, or in reverse order
+
+# %%
+with create_graphs():
+    create_basis(
+        fn=mu2d.compose(
             [
                 mu2d.rotate(math.radians(53.130102)),
                 mu2d.translate(mu2d.Vector2D(x=1.0, y=0.0)),
@@ -178,4 +207,40 @@ with create_graphs("foo", "bar.txt", (3, 3)):
         ),
     )
 
+# %% [markdown]
+# Composed functions, read bottom up
+# ----------------------------------
+#
+# The sequence of functions shown, where the translate
+# is applied first, and operations are relative to the
+# units on the left and bottom.
+
 # %%
+for f in mu.compose_intermediate_fns(
+    [
+        mu2d.rotate(math.radians(53.130102)),
+        mu2d.translate(mu2d.Vector2D(x=1.0, y=0.0)),
+    ]
+):
+    with create_graphs():
+        create_basis(fn=f)
+
+# %% [markdown]
+# Composed functions, read top down
+# ---------------------------------
+#
+# The sequence of functions shown, where we visualize
+# the rotate first, and then the translate relative to
+# that relative graph paper.
+#
+
+# %%
+for f in mu.compose_intermediate_fns(
+    [
+        mu2d.rotate(math.radians(53.130102)),
+        mu2d.translate(mu2d.Vector2D(x=1.0, y=0.0)),
+    ],
+    relative_basis=True,
+):
+    with create_graphs():
+        create_basis(fn=f)
