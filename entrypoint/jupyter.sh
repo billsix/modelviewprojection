6420f3ebfc72d59bd782e2baf564a3d3362681b2
William Emerison Six <billsix@gmail.com>
2025-11-10 23:40:02 -0500

    still not working

--- entrypoint/jupyter.sh
+++ entrypoint/jupyter.sh
@@ -1,11 +1,14 @@
 cd /mvp/
 # in general this is super dangerous, but for our purposes,
 # it's fine
-exec  jupyter lab \
+exec 
+
+  jupyter lab \
          --allow-root \
          --ip=0.0.0.0 \
          --port=8888 \
-         --no-browser \
-         --NotebookApp.token='' \
-         --NotebookApp.password=''
+         --ServerApp.token='' \
+         --ServerApp.password='' \
+         --ServerApp.disable_check_xsrf=True \
+         --no-browser 
 
