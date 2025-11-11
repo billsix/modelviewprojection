74e62bc883eac009ffd82d9722c9f0e6049794df
William Emerison Six <billsix@gmail.com>
2025-11-10 23:04:10 -0500

    see if this works

--- entrypoint/jupyter.sh
+++ entrypoint/jupyter.sh
@@ -1,2 +1,11 @@
 cd /mvp/
-exec jupyter lab --allow-root
+# in general this is super dangerous, but for our purposes,
+# it's fine
+exec  jupyter notebook \
+         --allow-root \
+         --ip=0.0.0.0 \
+         --port=8888 \
+         --no-browser \
+         --NotebookApp.token='' \
+         --NotebookApp.password=''
+
