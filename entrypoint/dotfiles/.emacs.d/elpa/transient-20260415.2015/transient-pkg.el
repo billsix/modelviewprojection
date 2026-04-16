4889b4d34106fcf26e12116112efd86173e90822
William Emerison Six <billsix@gmail.com>
2026-04-15 22:01:27 -0400

    updated melpa repos

---
+++ entrypoint/dotfiles/.emacs.d/elpa/transient-20260415.2015/transient-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20260415.2015"
+  "Transient commands."
+  '((emacs    "28.1")
+    (compat   "30.1")
+    (cond-let "0.2")
+    (seq      "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "2e78a937413a33ac7fea0420041a1e55110f8002"
+  :revdesc "2e78a937413a"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
---
+++ entrypoint/dotfiles/.emacs.d/elpa/zenburn-theme-20260329.1838/zenburn-theme-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "zenburn-theme" "20260329.1838"
+  "A low contrast color theme for Emacs."
+  ()
+  :url "https://github.com/bbatsov/zenburn-emacs"
+  :commit "1022240b1061369ecb605d55fa074d20d5c84aae"
+  :revdesc "1022240b1061"
+  :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com"))
+  :maintainers '(("Bozhidar Batsov" . "bozhidar@batsov.com")))
